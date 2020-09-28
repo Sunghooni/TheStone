53d9851d334618b5ba99f90aa5b52da75ede9b44
SungHoon <leesunghoon03@naver.com>
2020-09-28 17:19:47 +0900

    [ADD] teleport effect
    [ADD] - Teleport effect

--- Assets/Scripts/WizardCtrl.cs
+++ Assets/Scripts/WizardCtrl.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Xml;
 using UnityEngine;
 using UnityEngine.XR;
@@ -14,7 +15,10 @@ public class WizardCtrl : MonoBehaviour
     private float horz;
     private float vert;
     private float moveSpeed = 5;
-    private float rotSpeed = 3;
+    private float rotSpeed = 2;
+
+    public ParticleSystem TeleportPtc;
+    public bool movable = true;
 
     void Start()
     {
@@ -29,24 +33,21 @@ public class WizardCtrl : MonoBehaviour
 
     private void FixedUpdate()
     {
-        MoveCtrl();
+        if(movable)
+            MoveCtrl();
     }
 
     void MouseClick()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, 30))
             {
-                if (hit.transform.tag == "Fixed")
+                if (hit.transform.tag == "Untagged")
                 {
-                    Teleport(hit.transform.gameObject);
-                    horz = 0;
-                    vert = 0;
+                    Debug.Log("Movable");
                 }
-                else
-                    Debug.Log("It isn't able to Climb");
             }
         }
     }
@@ -63,11 +64,47 @@ public class WizardCtrl : MonoBehaviour
             anim.SetBool("run", true);
         else
             anim.SetBool("run", false);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            TeleportCheck();
     }
 
-    void Teleport(GameObject obj)
+    void TeleportCheck()
     {
-        var pos = obj.transform.position;
-        this.transform.position = new Vector3(pos.x, pos.y + 1.5f, pos.z);
+        var mePos = this.transform.position;
+
+        if (Physics.Raycast(new Vector3(mePos.x, mePos.y + 1, mePos.z), transform.forward * 5, out hit))
+        {
+            if (hit.transform.tag == "Fixed")
+                StartCoroutine(TeleportEft());
+            else
+                Debug.Log("It isn't able to Climb");
+        }
+    }
+
+    IEnumerator TeleportEft()
+    {
+        Debug.Log("1");
+        float timer = 0;
+        TeleportPtc.Play();
+
+        movable = false;
+        anim.SetBool("run", false);
+
+        while (true)
+        {
+            timer += Time.deltaTime;
+            if (timer > 1.5f)
+            {
+                var toPos = hit.transform.position;
+                this.transform.position = new Vector3(toPos.x, toPos.y + 1.5f, toPos.z);
+            }
+            if(timer > 3)
+            {
+                movable = true;
+                break;
+            }
+            yield return null;
+        }
     }
 }
