0d30fee6196379e3bd4f7ea737871b760c8814e4
SungHoon <leesunghoon03@naver.com>
2020-10-18 18:00:20 +0900

    [ADD] ArrowMotion.cs
    [ADD] - ArrowMotion.cs

---
+++ Assets/Scripts/ArrowMotion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMotion : MonoBehaviour
+{
+    float timer = 0;
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        transform.Rotate(Vector3.up * 1);
+
+        if (timer < 1)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime);
+        }
+        else if (timer > 1 && timer < 2)
+        {
+            transform.Translate(-Vector3.up * Time.deltaTime);
+        }
+        else
+            timer = 0;
+    }
+}
