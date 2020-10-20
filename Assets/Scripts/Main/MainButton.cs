647e8ab9b6cc21f784c8612733754235c8280d25
SungHoon <leesunghoon03@naver.com>
2020-10-20 19:04:39 +0900

    [ADD] LoadTutorial
    [ADD] - LoadTutorial

--- Assets/Scripts/Main/MainButton.cs
+++ Assets/Scripts/Main/MainButton.cs
@@ -1,21 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainButton : MonoBehaviour
 {
     public void PlayBtnOnclick()
     {
-
+        Debug.Log("PlayBtn");
+        SceneManager.LoadScene("Tutorial");
     }
 
     public void StageBtnOnclick()
     {
-
+        Debug.Log("StageBtn");
     }
 
     public void OptionBtnOnclick()
     {
-
+        Debug.Log("OptionBtn");
     }
 }
