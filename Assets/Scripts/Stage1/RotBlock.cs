5adc5592f2d76b008247da6292feda02bc3b0825
SungHoon <leesunghoon03@naver.com>
2020-10-24 20:22:25 +0900

    [ADD] RotBlock_Script/Setting
    [ADD] - RotBlock_Script/Setting

--- Assets/Scripts/Stage1/RotBlock.cs
+++ Assets/Scripts/Stage1/RotBlock.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class RotBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector3 rotPoint;
+
+    void Awake()
     {
+        var pos = this.transform.position;
+        rotPoint = new Vector3(pos.x - 1.5f, pos.y - 1.5f, pos.z);
+        StartCoroutine(RotMovement());
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //RotAround();
+        Debug.DrawRay(rotPoint, Vector3.forward * 1, Color.red, 2);
+    }
+
+    IEnumerator RotMovement()
+    {
+        while(true)
+        {
+            if (this.transform.eulerAngles.z > 90)
+                break;
+            transform.RotateAround(rotPoint, Vector3.forward, Time.deltaTime);
+            yield return new FixedUpdate();
+        }
     }
 }
