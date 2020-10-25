73c2e24050931dbd5267b21d692c7f0903cd39c8
SungHoon <leesunghoon03@naver.com>
2020-10-25 21:25:07 +0900

    [ADD] TriggerBlock_Script
    [ADD] - TriggerBlock_Script

--- Assets/Scripts/Stage1/RotBlockCtrl.cs
+++ Assets/Scripts/Stage1/RotBlockCtrl.cs
@@ -7,4 +7,13 @@ public class RotBlockCtrl : MonoBehaviour
     public GameObject[] RotBlocks;
     public GameObject[] TrigggerBlocks;
     float timer = 0;
+
+    void CheckTrigger()
+    {
+        for(int i = 0; i < TrigggerBlocks.Length; i++)
+        {
+            if (RotBlocks[i].tag != "Fixed")
+                break;
+        }
+    }
 }
