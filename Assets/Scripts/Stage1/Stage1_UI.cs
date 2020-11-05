bf0a965474495afed513aeae332709610fd5158e
SungHoon <leesunghoon03@naver.com>
2020-11-05 17:35:36 +0900

    [ADD] ClearUI_Script
    [ADD] - ClearUI_Script

---
+++ Assets/Scripts/Stage1/Stage1_UI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stage1_UI : MonoBehaviour
+{
+    public GameObject ClearCircle;
+    public GameObject ClearUI;
+    public Text sideText;
+
+    private void Update()
+    {
+        ShowClearUI();
+    }
+
+    private void ShowClearUI()
+    {
+        if(!ClearUI.activeSelf && ClearCircle.GetComponent<CheckClear>().isClear)
+        {
+            ClearUI.SetActive(true);
+        }
+    }
+}
